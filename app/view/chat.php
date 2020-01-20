64d97470847554a7fa4e3cb08dc4fc26c85f705a
MushiX CSys <admin@mushix.eu>
2020-01-20 19:06:45 +0100

    Add firebase service

--- app/view/chat.php
+++ app/view/chat.php
@@ -5,5 +5,17 @@ if(!isset($_SESSION['initiate']) || !isset($_SESSION['login']) || $_SESSION['is_
     header("Location: ../view/login.php");
 }
 
+require '../../vendor/autoload.php';
+
+use Kreait\Firebase\Factory;
+
+$factory = (new Factory)
+  ->withServiceAccount('../../ahechat-91c01be9bd8b.json')
+  ->withDatabaseUri('https://ahechat.firebaseio.com');
+
+$database = $factory->createDatabase();
+
+
+
 echo 'Welcome to chat';
 ?>
